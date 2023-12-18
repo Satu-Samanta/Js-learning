e2d8b88f29cbe730d00a837bead3569ecac341aa
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-18 04:28:59 +0000

    add1

--- 01_Object/Tutorial_2nd.js
+++ 01_Object/Tutorial_2nd.js
@@ -13,7 +13,18 @@ const regularUser = {
     email : " someone@gmail.com",
     fullname :{
         userfullname : {
-            firstnam:
+            firstnam:"Hitesh",
+            lastname: "Choudhary"
         }
     }
 }
+
+//console.log(regularUser.fullname.userfullname.firstnam);
+
+
+//***object merge****
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+//const obj3 = {obj1, obj2}
+//console.log(obj3);
