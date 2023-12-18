1b482b7ceb6a560119d3d99ebef2ac81d48fe0cc
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-18 04:37:20 +0000

    commit

--- 01_Object/Tutorial_2nd.js
+++ 01_Object/Tutorial_2nd.js
@@ -28,3 +28,14 @@ const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"a",4:"b"}
 //const obj3 = {obj1, obj2}
 //console.log(obj3);
+
+//const obj3 = Object.assign({},obj1, obj2)
+
+//console.log(obj3);
+//console.log(obj3[2]);
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+
+
+
