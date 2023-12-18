528c2e9d09efa57d84ad93ec50e4d21e7996350d
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-18 04:15:11 +0000

    ADD

---
+++ 01_Object/Tutorial_2nd.js
@@ -0,0 +1,19 @@
+// Single ton & constructor
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sam"
+tinderUser.isloggedIn = false
+
+console.log(tinderUser);
+
+
+const regularUser = {
+    email : " someone@gmail.com",
+    fullname :{
+        userfullname : {
+            firstnam:
+        }
+    }
+}
