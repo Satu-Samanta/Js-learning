66dce55c489d9a863870951d3cad7b6fd0819f2d
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-21 04:52:56 +0000

    for in/of loop

---
+++ High_order_array_loops/Loops.js
@@ -0,0 +1,86 @@
+// //for of
+
+// //["","",""] array string
+// //[{},{},{}] array object
+// const arr=[1,2,3,4,5]
+
+// // for (const iterator of object) {
+    
+// // }
+
+// //for (const num of arr) {
+//     //console.log(num);
+    
+// //}
+
+// const greetings="Hello world"
+
+// for (const greet of  greetings) {
+//     //console.log(`Each chsr is ${greet}`)
+    
+// }
+
+// //Maps: Uniqe value and maitain the order
+// // const map = new Map()
+// // map.set('IN',"India")
+// // map.set('USA',"United state of america")
+// // map.set('Fr',"France")
+
+// // console.log(map);
+// // case:1
+
+// // const map = new Map()
+// // map.set('IN',"India")
+// // map.set('USA',"United state of america")
+// // map.set('Fr',"France")
+// // //map.set('IN',"India")
+// //console.log(map);
+
+// // for (const key of map) {
+// //     console.log(key);   
+// // }
+// //Array destructuring
+// //for (const [key,value] of map) {
+//     //console.log(key, ':-' , value) ;   
+// //}
+// //We cant use for of loop in object:--->
+// // const myObject={
+// //     game1:'NFS'
+// //     game2:'Spiderman'
+// // }
+
+// // for (const [key,value] of myObject) {
+// //     console.log(key, ':-' , value) ;   
+// //}
+
+// const myObject={
+//     js: 'Javascript',
+//     cpp:'C++',
+//     rb:'Ruby',
+//     swift:"swift by apple"
+// }
+
+// //for (const key in myObject) {
+//         //console.log(key);   
+//         //console.log(myObject[key]);  
+//         //console.log(`${key} shortcut is for ${myObject[key]}`); 
+// }
+
+// const programming = ["js","rb","py","java","cpp"]
+
+// //for (const key in programming) {
+   
+//    // console.log(key);  
+//  //  console.log(programming[key]); 
+//    // }
+
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United state of america")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
