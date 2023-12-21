3486b455f13d86c35a6380e381cceec4b18c9b6e
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-21 03:25:10 +0000

    Pushing arrow function

--- Function/Arrow_function.js
+++ Function/Arrow_function.js
@@ -5,7 +5,51 @@ const user = {
     price: 999,
     welcomeMessage: function(){
         console.log(`${this.username},Welcome to website`);
+        console.log(this);
     }
 }
-user.welcomeMessage()
+
+//user.welcomeMessage()
+//user.username="sam"
+//user.welcomeMessage()
+//console.log(this);//output it will give  {}
+
+// function chai(){
+//     let username="hitesh"
+//     console.log(this.usernam);
+// }
+//chai()// out put :undefined,we cant use this in fuction.
+
+//*****************/
+// const chai = function (){
+//        let username="hitesh"
+//          console.log(this.usernam);
+//     }
+// chai()
+
+//************* */
+//     const chai = ()=>{
+//         let username="hitesh"
+//           console.log(this.usernam);
+//      }
+//  chai()
+
+// const addTwo = (num1,num2) =>{
+//     return num1+num2
+// }
+// console.log(addTwo(3,4))
+ 
+//const addTwo = (num1,num2) =>num1+num2
+
+
+////-----///
+// const addTwo = (num1,num2) =>(num1+num2)
+
+//     console.log(addTwo(3,4))
+///---///
+
+const addTwo = (num1,num2) =>({username:"Hitesh"})//passing objct
+
+    console.log(addTwo(3,4))
+
 
