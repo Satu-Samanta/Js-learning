1bd3cbc167b7c523c58c833b132d00fc02e3037b
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-22 03:52:29 +0000

    modified

--- High_order_array_loops/01_filter.js
+++ High_order_array_loops/01_filter.js
@@ -14,7 +14,8 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 //     61, 71, 81, 91, 101
 //   ]
 //else
-const newnums= myNumbers.map( (num) => num *10)
+const newnums= myNumbers
+.map( (num) => num *10)
 .map( (num) =>num+1)
 .filter((num)=>num>=40)
 
