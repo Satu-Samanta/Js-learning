dae5dffd3b53da957746978f5a15b648ff61787b
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2023-12-22 02:26:57 +0000

    a

--- High_order_array_loops/last.js
+++ High_order_array_loops/last.js
@@ -1,4 +1,4 @@
-const myNums = [1,2,3,4,5,6,7,8,9,10]
+//const myNums = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNums = myNums.filter((num) => num>4)
 // console.log(newNums);//[ 5, 6, 7, 8, 9, 10 ]
@@ -11,11 +11,39 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 // //it is giving the rturn value which can be tored.
 // console.log(newNums);//[ 5, 6, 7, 8, 9, 10 ]
 
-const newNums = []
+//*********** */
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = []
 
-myNums.forEach( (num) => {
-    if(num > 4){
-        newNums.push(num)
-    }
-})
-console.log(newNums);//[ 5, 6, 7, 8, 9, 10 ]
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);//[ 5, 6, 7, 8, 9, 10 ]
+
+/******************/
+
+const book = [
+    {title: 'Book one',genre: 'Fiction', publish: 1981,
+     edition: 2004},
+     {title: 'Book Two',genre: 'Non-Fiction', publish: 1992,
+     edition: 2008},
+     {title: 'Book Three',genre: 'Fiction', publish: 1999,
+     edition: 2007},
+     {title: 'Book Four',genre: 'Non-Fiction', publish: 1989,
+     edition: 2010},
+     {title: 'Book Five',genre: 'Science', publish: 2009,
+     edition: 2014},
+     {title: 'Book Six',genre: 'Fiction', publish: 1987,
+     edition: 2010},
+     {title: 'Book Seven',genre: 'History', publish: 1986,
+     edition: 1996},
+     {title: 'Book Eight',genre: 'Science', publish: 2011,
+     edition: 2016},
+     {title: 'Book Nine',genre: 'Non-Fiction', publish: 1981,
+     edition: 1989}
+]
+const userbooks = bool.filter( () =>)
+
+console.log(userbooks);
