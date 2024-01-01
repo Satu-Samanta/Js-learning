73537df1eac2ea9e0f41bb2a4157d11d063b7170
Satu-Samanta <151837743+Satu-Samanta@users.noreply.github.com>
2024-01-01 14:12:14 +0000

    Add1

--- Function/tst.js
+++ Function/tst.js
@@ -9,31 +9,31 @@
 // //const obj3 = {...obj1,...obj2}
 // console.log(obj3);
 
-const users = [
-    {firstName : "Akshay", lastName: "Saini", age:26},
-    {firstName : "Donal", lastName: "Trumph", age:75},
-    {firstName : "Elon", lastName: "Musk", age:50},
-    {firstName : "Deepika", lastName: "pADUKON", age:26},
-]
+// const users = [
+//     {firstName : "Akshay", lastName: "Saini", age:26},
+//     {firstName : "Donal", lastName: "Trumph", age:75},
+//     {firstName : "Elon", lastName: "Musk", age:50},
+//     {firstName : "Deepika", lastName: "pADUKON", age:26},
+// ]
 
 // const output = users.reduce(function(acc,curr){
 //     console.log(`accumulator :${[curr.age]} and current value ${curr}`);
 //     console.log(acc[curr.age]);
 // return acc;
 // },{})
-const output = users.reduce(function(acc,curr){
-    console.log(acc);
-if(acc[curr.age]){
+// const output = users.reduce(function(acc,curr){
+//     console.log(acc);
+// if(acc[curr.age]){
    
-    acc[curr.age] = ++acc[curr.age];
-}else{
-    acc[curr.age] = 1;
+//     acc[curr.age] = ++acc[curr.age];
+// }else{
+//     acc[curr.age] = 1;
   
-}
-return acc;
-},[]);
+// }
+// return acc;
+// },[]);
 
-// console.log(output);
+// // console.log(output);
 
 
 // console.log(users.firstName); // Output: Akshay
@@ -60,3 +60,22 @@ return acc;
 // }, {});
 
 // console.log(ageGroupedUsers);
+
+
+// const numbers =[65,44,12,4];
+// const newArray = numbers.forEach((items,index,arr)=>arr[index] = items*10)
+// console.log(numbers,newArray);
+
+
+// @Satu-Samanta ➜ /workspaces/Js-learning/Function (main) $ node tst.js
+// [ 650, 440, 120, 40 ] undefined
+
+const arr = [2,5,3,4,7];
+const mapRsult = arr.map((arr)=>arr+2)
+
+// const forEachResult = arr.forEach((ar,i)=> arr[i]=ar*10);
+// console.log(mapRsult,forEachResult,arr);
+// @Satu-Samanta ➜ /workspaces/Js-learning/Function (main) $ node tst.js
+// [ 4, 7, 5, 6, 9 ] undefined [ 20, 50, 30, 40, 70 ]
+// @Satu-Samanta ➜ /workspaces/Js-learning/Function (main) $ 
+
